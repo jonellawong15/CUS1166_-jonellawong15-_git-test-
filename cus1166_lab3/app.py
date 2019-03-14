5490a6c4b8bc92c6d0c9884ebf782a992fcf64a2
jonellawong15 <47012063+jonellawong15@users.noreply.github.com>
2019-03-14 10:20:53 -0400

    Update app.py

--- cus1166_lab3/app.py
+++ cus1166_lab3/app.py
@@ -1,1 +1,21 @@
+<head> Welcome! 
+</head>
+
+{% extends 'layout.html' %}
+{% block body %}
+<form action="{{ url_for('messenger') }}" method="post">
+<input type="text" name="name" placeholder="Enter your name">
+<input type="text" name="message" placeholder="Type in a course">
+<button type="Submit"> Send Message</button>
+</form>
+{% endblock %}
+
+def add_course():
+  name = request.form.get("name")
+  message = request.form.get("course")
+  return render_template("course_details.html", name=name, course=course)
+
+def register_student():
+  {% get {{student_name}} %}
+
 
